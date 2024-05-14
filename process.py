6fa171355bac9a0ee3900c122409dfe3b6813f9d
hwu27 <huw029@ucsd.edu>
2024-05-14 14:14:47 -0700

    cleaned up and added space after column commas

--- process.py
+++ process.py
@@ -7,13 +7,15 @@ def process_csv(input_file_path, output_file_path):
         data = file.read()
     data = re.sub(r'\s*,\s*', ',', data) # removes all spaces around commas
     
-    with open('./output_data/temp.csv', 'w') as file:
-        file.write(data)
+    with open('./output_data/temp.csv', 'w') as file: 
+        file.write(data) # use temp file for safety first
     
     df = pd.read_csv('./output_data/temp.csv', header=None, on_bad_lines='skip')
     df = df[df.count(axis=1) >= 3] # removes rows with less than 3 columns
+    df = df.map(lambda x: re.sub(r'\s*,\s*', ',', str(x)))
     df = df.map(lambda x: re.sub(r'\s{2,}', ' ', str(x))) # remove consecutive spaces
     df = df.map(lambda x: re.sub(r'(?<=\w)(?!\.)\s([A-Z])', r'. \1', str(x))) # adds a period after a word that also has a capital letter word after it
+    df = df.map(lambda x: re.sub(r',(?=[^\s])', ', ', str(x))) # adds a space after a comma if it is inside the columns
     df.to_csv(output_file_path, index=False, header=False, quoting=csv.QUOTE_NONNUMERIC) # adds qutoes around every column
     file.close()
 
